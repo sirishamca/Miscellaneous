1b860ffa4af7c68112cfe8a838ea4eb95964a8e8
sirishamca <47160687+sirishamca@users.noreply.github.com>
2019-02-12 11:54:20 -0600

    Update polymorphism.go

--- polymorphism.go
+++ polymorphism.go
@@ -2,22 +2,48 @@ package main
 
 import "fmt"
 
-type Parent interface {
-	M1() string
+type Hobby interface {
+	myStereotype() string
 }
 
-type ChildClass struct {
-	name string
+type Human struct {
 }
 
-func (t ChildClass) M1() string {
-	return t.name
+func (h Human) myStereotype() string {
+	return "I'm a Human, only an abstract concept, and I can have no hobby."
 }
 
-func Hello(p Parent) {
-	fmt.Printf("Hi, my name is %s\n", p.M1())
+type Man struct {
+	Human //anonymous class to inherit Human behavior
+}
+
+func (m Man) myStereotype() string {
+	return "I'm a Man and I'm going fishing."
+}
+
+type Woman struct {
+	Human //anonymous class to inherit Human behavior
+}
+
+type Dog struct {
+	//does not inherit any other type
+}
+
+func (m Dog) myStereotype() string {
+	return "bow bow bow"
 }
 
 func main() {
-	Hello(ChildClass{name: "Sirisha"})
+	h := new(Human)
+	m := new(Man)
+	w := new(Woman)
+	d := new(Dog)
+
+	//an array of hobby instances - we don’t need to know whether human or dog
+	hobbyArr := [...]Hobby{h, m, w, d} //array of 3 Humans and 1 dog.
+	for n, _ := range hobbyArr {
+
+		fmt.Println("My hobby?  Well,", hobbyArr[n].myStereotype()) //appears as Hobby type, but behavior changes depending on actual instance
+
+	}
 }
